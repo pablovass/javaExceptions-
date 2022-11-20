b6f2278bf681ee6ba240e319ff0b3e253f03a892
pablovass <faktory.fm@live.com>
2022-11-20 17:54:20 -0300

    generico

--- promesasJava/src/main/java/com/pablovass/genericos/App.java
+++ promesasJava/src/main/java/com/pablovass/genericos/App.java
@@ -2,7 +2,9 @@ package com.pablovass.genericos;
 
 public class App {
     public static void main(String[] args) {
-        Clase<String> c = new Clase<String>("pablovass");
+     //   Clase<String> c = new Clase<String>("pablovass");
+     //   c.mostrarTipo();
+        Clase<String,Integer,Double,String>c =new Clase<>("hola",32,"pablo",32.1);
         c.mostrarTipo();
     }
 }
--- promesasJava/src/main/java/com/pablovass/genericos/Clase.java
+++ promesasJava/src/main/java/com/pablovass/genericos/Clase.java
@@ -1,13 +1,26 @@
 package com.pablovass.genericos;
 
-public class Clase<T> {
-    private T objeto;
+public class Clase<T, K, V, E> {
+    private T objetoT;
+    private K objetoK;
+    private E objetoE;
+    private V objetoV;
 
-    public Clase(T objeto) {
-        this.objeto = objeto;
+    public Clase(T objetoT, K objetoK, E objetoE, V objetoV) {
+        this.objetoT = objetoT;
+        this.objetoK = objetoK;
+        this.objetoE = objetoE;
+        this.objetoV = objetoV;
+    }
+
+    public Clase() {
+
     }
 
     public void mostrarTipo() {
-        System.out.println("T es un: " + objeto.getClass().getName()); //muestra el nombre la clase que va implementar esta clase
+        System.out.println("T es un: " + objetoT.getClass().getName()); //muestra el nombre la clase que va implementar esta clase
+        System.out.println("K es un: " + objetoK.getClass().getName());
+        System.out.println("E es un: " + objetoE.getClass().getName());
+        System.out.println("V es un: " + objetoV.getClass().getName());
     }
 }
