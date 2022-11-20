d3f341cac364796c4b38bed5f72443514f97b132
pablovass <faktory.fm@live.com>
2022-11-20 19:01:45 -0300

    promesas en java

--- promesasJava/src/main/java/com/pablovass/promesas/CHilo.java
+++ promesasJava/src/main/java/com/pablovass/promesas/CHilo.java
@@ -1,5 +1,12 @@
 package com.pablovass.promesas;
 
-public class CHilo {
-    // comentarios
+import java.util.concurrent.Callable;
+//la diferencia entre Runnable y Callable que que Callable retorma valor y el otro no retorna valor
+public class CHilo  implements Callable<String> {
+    // este retorna valor
+    @Override
+    public String call() throws Exception {
+        return "hilo Callable";
+    }
+
 }
