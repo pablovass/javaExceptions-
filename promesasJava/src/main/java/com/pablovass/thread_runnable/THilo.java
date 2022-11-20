a0f864573b6489673cc580a4af6c7d87b9b9eef8
pablovass <faktory.fm@live.com>
2022-11-20 18:13:44 -0300

    multi hilos

---
+++ promesasJava/src/main/java/com/pablovass/thread_runnable/THilo.java
@@ -0,0 +1,17 @@
+package com.pablovass.thread_runnable;
+// si usas thread usaamos herencia
+public class THilo extends Thread {
+    private int id;
+
+    public THilo(int id) {
+        this.id = id;
+    }
+
+    //para poder implementar la logica de una hilo tengo que sobrescribir el metodo run
+    @Override
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("ejecutando logica de hilo de id" + id);
+        }
+    }
+}
